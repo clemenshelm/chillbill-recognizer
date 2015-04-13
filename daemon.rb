596ffcad55a943946a5df6baea7c99d52dbea99f
Clemens Helm <clemens.helm@gmail.com>
2015-04-13 11:23:03 +0200

    Treat unprocessed bills as own collection and track removed bills.

--- daemon.rb
+++ daemon.rb
@@ -9,18 +9,18 @@ EventMachine.run do
 
   ddp_client.onconnect = lambda do |event|
     puts 'connected'
-    ddp_client.subscribe('unprocessed-bills', []) do |result|
-      puts 'result'
-      posts = ddp_client.collections['bills']
-      puts ddp_client.collections
-      #EM.stop_event_loop
-    end
 
-    ddp_client.observe 'bills', %w(added changed) do |id, bill|
+    ddp_client.subscribe('unprocessed-bills', [])
+
+    ddp_client.observe 'unprocessed-bills', 'added' do |id, bill|
       puts "bill was added: #{bill}"
       # TODO: process bills
       bill_attributes = {}
       ddp_client.call :writeDetectionResult, [id, bill_attributes]
+    end
+
+    ddp_client.observe 'unprocessed-bills', 'removed' do |id|
+      puts "bill was removed: #{id}"
     end
   end
 end
