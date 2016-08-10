225b57c14f2dc1dd7700569033976c3688f8ce8c
Pilar Huidobro <huidobropilar@gmail.com>
2016-08-10 11:32:56 +0200

    Allow retrieving for all file types and logging

--- lib/bill_image_retriever.rb
+++ lib/bill_image_retriever.rb
@@ -2,6 +2,7 @@ require 'tempfile'
 require 'grim'
 require 'open-uri'
 require 'aws-sdk'
+require '../config/logger.rb'
 
 class BillImageRetriever
   def initialize(url:)
@@ -11,15 +12,31 @@ class BillImageRetriever
   def save
     _, bucket, region, key = @url.match(%r{^https://([^\.]+)\.s3[-\.]([^\.]+).amazonaws.com/(.+)$}).to_a
     puts "bucket: #{bucket}, region: #{region}, key: #{key}"
-    pdf_file = Tempfile.new ['bill', '.pdf']
-    s3 = Aws::S3::Client.new(region: region)
-    s3.get_object(bucket: bucket, key: key, response_target: pdf_file)
 
-    image_file = Tempfile.new ['bill', '.png']
-    pdf = Grim.reap(pdf_file.path)
-    pdf[0].save(image_file.path, width: 3000, quality: 100)
-    pdf_file.close!
+    file_basename = File.basename key
+    file_extension = File.extname  file_basename
+    bill_id = File.basename file_basename, file_extension
 
-    image_file
+    case file_extension
+    when ".pdf"
+      pdf_file = Tempfile.new ['bill', '.pdf']
+      s3 = Aws::S3::Client.new(region: region)
+      s3.get_object(bucket: bucket, key: key, response_target: pdf_file)
+
+      image_file = Tempfile.new ['bill', '.png']
+      pdf = Grim.reap(pdf_file.path)
+      pdf[0].save(image_file.path, width: 3000, quality: 100)
+      pdf_file.close!
+
+      image_file
+    when ".png", ".jpg", ".jpeg"
+      image_file = Tempfile.new ['bill', file_extension]
+      s3 = Aws::S3::Client.new(region: region)
+      s3.get_object(bucket: bucket, key: key, response_target: image_file)
+
+      image_file
+    else
+      logger.warning("Unknow data type")
+    end
   end
 end
