c31c55885f978122ab62c83ca0726ce5f855b619
Pilar Huidobro <huidobropilar@gmail.com>
2016-09-20 11:00:10 +0200

    remove duplicate test

--- spec/calculations/date_calculation_spec.rb
+++ spec/calculations/date_calculation_spec.rb
@@ -5,20 +5,6 @@ describe DateCalculation do
     DateTerm.dataset.delete
   end
 
-# This test is still broken:
-# Failure/Error: expect(dates.invoice_date).to eq DateTime.iso8601('2015-04-01')
-#    NoMethodError:
-#      undefined method `all' for [#<Double (anonymous)>, #<Double (anonymous)>]:Array
-#      Did you mean?  all?
-  it 'uses the first date as invoice date' do
-    words = %w(2015-04-01 2015-02-28)
-      .map { |date_string| DateTime.iso8601(date_string) }
-      .map { |datetime| double(to_datetime: datetime)  }
-
-    dates = DateCalculation.new(words)
-    expect(dates.invoice_date).to eq DateTime.iso8601('2015-04-01')
-  end
-
   it 'returns nil if there is no invoice date candidate' do
     dates = DateCalculation.new([])
     expect(dates.invoice_date).to be_nil
