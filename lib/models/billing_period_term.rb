9da7847508894800651d3c23821f9de7582e64b6
Pilar Huidobro <huidobropilar@gmail.com>
2016-10-06 11:44:37 +0200

    write billing period term test and fix billing period term

--- lib/models/billing_period_term.rb
+++ lib/models/billing_period_term.rb
@@ -1,13 +1,9 @@
 require 'sequel'
 require_relative '../boot'
 require_relative './term_builder'
-require_relative './date_term'
 
 class BillingPeriodTerm < Sequel::Model
+  require_relative './date_term' # Loading it here resolves issues with the circular dependency
   many_to_one :from, class: DateTerm
   many_to_one :to, class: DateTerm
-
-  def to_isoperiod
-    
-  end
 end
--- spec/models/billing_period_term_spec.rb
+++ spec/models/billing_period_term_spec.rb
@@ -1,9 +1,42 @@
 require_relative '../../lib/boot'
 require_relative '../../lib/models/billing_period_term'
+require_relative '../../lib/models/date_term'
+require_relative '../../lib/models/word'
+require_relative '../support/factory_girl'
+require_relative '../factories'
 
 describe BillingPeriodTerm do
-  it "returns the correct values for a billing period" do
-    term = BillingPeriodTerm.new(text: '13.04.15')
-    expect(term.to_datetime).to eq DateTime.iso8601('2015-04-13')
+  before(:each) do
+    Word.dataset.delete
+    BillingPeriodTerm.dataset.delete
+    DateTerm.dataset.delete
+  end
+
+  it "sets the correct values for a billing period" do
+    start_of_period = DateTerm.create(
+      text: "01.03.2015",
+      left: 591,
+      right: 798,
+      top: 773,
+      bottom: 809,
+      first_word_id: 19
+    )
+
+    end_of_period = DateTerm.create(
+      text: "31.03.2015",
+      left: 832,
+      right: 1038,
+      top: 773,
+      bottom: 809,
+      first_word_id: 26
+    )
+
+    billing_period = BillingPeriodTerm.create(
+      from: start_of_period,
+      to: end_of_period
+    )
+
+    expect(billing_period.from).to eq start_of_period
+    expect(billing_period.to).to eq end_of_period
   end
 end
