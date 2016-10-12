0d3f4106fd8ca354381aeaccdede3327ae13792d
Aleks <markovic.aleks@outlook.de>
2016-10-12 14:23:10 +0200

    Corrected mistakes

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/boot'
 require_relative '../../lib/models/word'
 
-describe 'recognizing words' do
+describe 'recognizing words', :focus do
   Word.dataset.delete
 
   it 'next function works properly' do
@@ -11,10 +11,17 @@ describe 'recognizing words' do
     expect(word1.next).to eq word2
   end
 
-  it 'follows function does work' do
-    word1 = Word.create(text: "Hello", left: 100, right: 250, top: 250, bottom: 300)
-    word2 = Word.create(text: "World", left: 120, right: 270, top: 270, bottom: 320)
+  it 'one word follows the other' do
+    word1 = Word.create(text: "IHREN", left: 434, right: 526, top: 1684, bottom: 1714)
+    word2 = Word.create(text: "EINKAUF!", left: 547, right: 690, top: 1684, bottom: 1714)
 
-    expect(word1.follows(word2)).to eq false
+    expect(word2.follows(word1)).to eq true
+  end
+
+  it 'one word does not follow the other' do
+    word1 = Word.create(text: "Danke!", left: 326, right: 179, top: 300, bottom: 330)
+    word2 = Word.create(text: "Betrag", left: 400, right: 324, top: 315, bottom: 331)
+
+    expect(word2.follows(word1)).to eq false
   end
 end
