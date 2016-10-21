e8be7a99402c9dbf0bd3fc4cdec1db736492ee27
Pilar Huidobro <huidobropilar@gmail.com>
2016-10-21 16:21:33 +0200

    fix dimensionable right after

--- lib/models/dimensionable.rb
+++ lib/models/dimensionable.rb
@@ -27,7 +27,7 @@ module Dimensionable
       all.find do |following|
         (following.left > current.right) &&
           (following.left - current.right) < (following.height * 10) &&
-          ((following.bottom <= (current.bottom * 1.0)) &&
+          ((following.bottom <= (current.bottom * 1.02)) &&
           ((current.bottom * 0.95) <= following.bottom))
       end
     end
