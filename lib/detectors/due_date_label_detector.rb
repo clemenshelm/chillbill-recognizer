968acd3e418b05b98e3348f696fc57b9e15ac709
Pilar Huidobro <huidobropilar@gmail.com>
2016-11-16 16:11:51 +0100

    create due date detector

---
+++ lib/detectors/due_date_label_detector.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require_relative '../models/due_date_label_term'
+
+class DueDateLabelDetector
+  DUE_DATE_LABELS =
+    /(?:^|(?<= ))(Zahlungstermin|Due\ Date:|Zahlungsziel:)(?:(?= )|$)/
+
+  def self.filter
+    find_due_date_labels(DUE_DATE_LABELS)
+    DueDateLabelTerm.dataset
+  end
+
+  def self.find_due_date_labels(regex, after_each_word: nil)
+    private
+    term = DueDateLabelTerm.new(regex: regex, after_each_word: after_each_word)
+    last_word = nil
+
+    Word.each do |word|
+      if term.exists? || (last_word && !word.follows(last_word))
+        term = DueDateLabelTerm.new(
+          regex: regex, after_each_word: after_each_word
+        )
+      end
+
+      term.add_word(word)
+
+      last_word = word
+
+      term.save if term.valid?
+    end
+  end
+end
