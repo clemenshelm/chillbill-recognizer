4d76e2c734e6712f125c36439ab995fc2771744c
Aleks <markovic.aleks@outlook.de>
2016-11-25 15:41:25 +0100

    corrected bill_recognition_spec

--- spec/acceptance/bill_recognition_spec.rb
+++ spec/acceptance/bill_recognition_spec.rb
@@ -3,14 +3,14 @@ require_relative '../spec_cache_retriever'
 require_relative '../../lib/bill_recognizer'
 
 describe 'Recognizing bills correctly' do
-  it 'recognizes the bill bsg8XJqLBJSt2dXeH' do
-    retriever = SpecCacheRetriever.new(file_basename: 'bsg8XJqLBJSt2dXeH.pdf')
+  it 'recognizes the bill BYnCDzw7nNMFergRW' do
+    retriever = SpecCacheRetriever.new(file_basename: 'BYnCDzw7nNMFergRW.pdf')
     recognizer = BillRecognizer.new(retriever: retriever)
 
     bill_attributes = recognizer.recognize
 
-    #expect(bill_attributes[:amounts]).to eq [{ total: 29_674, vatRate: 20 }]
-    expect(bill_attributes[:invoiceDate]).to eq '2016-10-01'
+    expect(bill_attributes[:amounts]).to eq [{ total: 29_674, vatRate: 20 }]
+    expect(bill_attributes[:invoiceDate]).to eq '2016-03-16'
     # TODO: Due date should actually be '2016-03-21'
     expect(bill_attributes[:dueDate]).to be_nil
     expect(bill_attributes[:vatNumber]).to eq 'ATU54441803'
