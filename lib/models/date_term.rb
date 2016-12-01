0055bf67fd93e66a11d2b66e4c4a1086ee3317c4
Aleks <markovic.aleks@outlook.de>
2016-12-01 12:11:45 +0100

    corrected mistakes

--- lib/models/date_term.rb
+++ lib/models/date_term.rb
@@ -54,8 +54,7 @@ class DateTerm < Sequel::Model
     when DateDetector::LONG_SLASH_DATE_REGEX
       DateTime.strptime(text, '%d/%m/%Y')
     when DateDetector::LONG_HYPHEN_DATE_REGEX
-      date_text = text.tr('-', '/')
-      DateTime.strptime(date_text, '%d/%m/%Y')
+      DateTime.strptime(text, '%d-%m-%Y')
     end
   end
 end
