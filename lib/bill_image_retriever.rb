25dcc3fdef012daf236fa087f1d702bcfdfc8c5e
Pilar Huidobro <huidobropilar@gmail.com>
2017-01-18 14:15:05 +0100

    Clean up url detail matching

--- lib/bill_image_retriever.rb
+++ lib/bill_image_retriever.rb
@@ -18,35 +18,31 @@ class BillImageRetriever
   include Magick
 
   def initialize(url:)
-    @url = url
+    _, @bucket, @region, @key = url.match(
+      %r{^https://([^\.]+)\.s3[-\.]([^\.]+).amazonaws.com/(.+)$}
+    ).to_a
+    logger.debug "bucket: #{@bucket}, region: #{@region}, key: #{@key}"
+
+    @file_extension = File.extname @key.downcase
   end
 
   def save
-    _, bucket, region, key = @url.match(
-      %r{^https://([^\.]+)\.s3[-\.]([^\.]+).amazonaws.com/(.+)$}
-    ).to_a
-    logger.debug "bucket: #{bucket}, region: #{region}, key: #{key}"
-
-    file_extension = File.extname key.downcase
-
-    @image_file = Tempfile.new ['bill', file_extension]
-
-    get_bill_from_s3(region, bucket, key)
-
-    determine_extension_validity(file_extension)
+    create_temp_file_and_download_bill_from_s3
+    determine_extension_validity
   end
 
-  def get_bill_from_s3(region, bucket, key)
-    s3 = Aws::S3::Client.new(region: region)
-    s3.get_object(bucket: bucket, key: key, response_target: @image_file)
+  def create_temp_file_and_download_bill_from_s3
+    @image_file = Tempfile.new ['bill', @file_extension]
+    s3 = Aws::S3::Client.new(region: @region)
+    s3.get_object(bucket: @bucket, key: @key, response_target: @image_file)
   end
 
-  def determine_extension_validity(file_extension)
-    case file_extension
+  def determine_extension_validity
+    case @file_extension
     when '.pdf', '.png', '.jpg', '.jpeg'
       @image_file
     else
-      raise UnprocessableFileError, file_extension
+      raise UnprocessableFileError, @file_extension
     end
   end
 end
