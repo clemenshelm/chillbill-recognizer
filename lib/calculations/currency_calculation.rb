a29f72627c669d350b3de5cc136d194ba42ddb54
Aleks <markovic.aleks@outlook.de>
2017-02-05 19:38:45 +0100

    fixed currency problem

--- lib/calculations/currency_calculation.rb
+++ lib/calculations/currency_calculation.rb
@@ -2,6 +2,14 @@
 class CurrencyCalculation
   def iso
     return nil if CurrencyTerm.empty?
-    CurrencyTerm.last.to_iso
+    currency_term = find_most_accurate_currency
+    currency_term&.to_iso
+  end
+
+  def find_most_accurate_currency
+    return nil if PriceTerm.empty?
+    most_accurate_currency_term = CurrencyTerm.below(PriceTerm.first)
+    most_accurate_currency_term ||= CurrencyTerm.right_after(PriceTerm.first)
+    most_accurate_currency_term || CurrencyTerm.first
   end
 end
