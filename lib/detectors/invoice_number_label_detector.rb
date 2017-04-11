60b2488cbd88ea451e4005bc6a9088146ae9e207
Pilar Huidobro <huidobropilar@gmail.com>
2017-04-11 15:22:06 +0200

    Rearrange regexes into an array

--- lib/detectors/invoice_number_label_detector.rb
+++ lib/detectors/invoice_number_label_detector.rb
@@ -2,20 +2,22 @@
 require_relative '../models/invoice_number_label_term'
 
 class InvoiceNumberLabelDetector
-  INVOICE_NUMBER_LABELS_REGEX = /(Re-Nr:|Bon-ID|Rech.Nr:|Beleg-nr.:)/
-  SINGLE_WORD_INVOICE_NUMBER_LABELS_REGEX =
-    /(Rechnungsnummer:|Rechnung:|Rechnungsnummer)/
+  INVOICE_NUMBER_LABELS= %w(
+    Re-Nr: Bon-ID Rech.Nr: Beleg-nr.: Rechnungsnummer: Rechnung: Rechnungsnummer
+  ).freeze
   MULTI_WORD_INVOICE_NUMBER_LABELS_REGEX =
     /(Invoice number:|Billing ID|Rechnung Nr.:)/
+
   def self.filter
+    invoice_number_label_regexes =
+      /#{INVOICE_NUMBER_LABELS.map { |s| Regexp.quote(s) }.join('|')}/
     end_word_with_space = ->(term) { term.text += ' ' }
 
     find_invoice_number_labels(
       MULTI_WORD_INVOICE_NUMBER_LABELS_REGEX,
       after_each_word: end_word_with_space
     )
-    find_invoice_number_labels(SINGLE_WORD_INVOICE_NUMBER_LABELS_REGEX)
-    find_invoice_number_labels(INVOICE_NUMBER_LABELS_REGEX)
+    find_invoice_number_labels(invoice_number_label_regexes)
 
     InvoiceNumberLabelTerm.dataset
   end
