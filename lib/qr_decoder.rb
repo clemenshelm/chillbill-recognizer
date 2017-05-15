b9a395c624f6cbe09006b2aab1a3f030894f3000
Clemens Helm <clemens@chillbill.co>
2017-05-15 17:05:33 +0200

    Add missing files.

---
+++ lib/qr_decoder.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require 'zbar'
+
+class QRDecoder
+  include Magick
+
+  def initialize(image)
+    @image = image
+  end
+
+  def qr_code?
+    tmp_image = @image.dup
+    image_blob = tmp_image.to_blob do
+      self.depth = 8
+      self.format = 'PGM'
+    end
+    tmp_image.destroy!
+
+    zbar_image = ZBar::Image.from_pgm(image_blob)
+
+    !zbar_image.process.empty?
+  end
+end
---
+++ spec/qr_decoder_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require_relative '../lib/qr_decoder'
+
+describe QRDecoder do
+  it 'detects that an image contains a QR code' do
+    image = Magick::Image.read('./spec/support/qr-test.pdf') { self.density = 600 }.first
+
+    decoder = QRDecoder.new(image)
+    expect(decoder.qr_code?).to be true
+  end
+
+  it 'detects that an image contains no QR code' do
+    image = Magick::Image.read('./spec/support/no-qr-test.pdf') { self.density = 600 }.first
+
+    decoder = QRDecoder.new(image)
+    expect(decoder.qr_code?).to be false
+  end
+
+  it 'does not manipulate the original image' do
+    image = Magick::Image.read('./spec/support/qr-test.pdf') { self.density = 600 }.first
+    original_image = image.dup
+
+    decoder = QRDecoder.new(image)
+    decoder.qr_code?
+
+    expect(image).to eq original_image
+  end
+end
