c5daab5d674cf6b94e026f94de2187605043b4fa
Pilar Huidobro <huidobropilar@gmail.com>
2017-07-13 14:29:02 +0200

    Make QR code data accessible, add vat rate extraction and return data in desired format

--- lib/qr_decoder.rb
+++ lib/qr_decoder.rb
@@ -16,20 +16,35 @@ class QRDecoder
     end
     tmp_image.destroy!
 
-    qr_codes = ZBar::Image.from_pgm(image_blob).process(symbology: :qrcode)
-    !qr_codes.empty?
+    @qr_codes = ZBar::Image.from_pgm(image_blob).process(symbology: :qrcode)
+    !@qr_codes.empty?
   end
 
   def decode_qr_code
     return unless qr_code?
-    all_data = @zbar_image.last.instance_variable_get(:@data).split('_')
+    all_data = @qr_codes.last.instance_variable_get(:@data).split('_')
+    price = all_data.find { |prices| /^(?!.*0,00).*$*\d,\d{2}/ =~ prices }
     processed_data = {
-      dueDate: all_data[4],
-      amounts: {
-        total: all_data[5] || all_data[6],
-        vatRate: all_data[5] ? 20 : 10
-      }
+      invoiceDate: DateTime.strptime(all_data[4], '%Y-%m-%d'),
+      dueDate: DateTime.strptime(all_data[4], '%Y-%m-%d'),
+      amounts: [{
+        total: (BigDecimal.new(price.sub!(',', '.')) * 100).to_i,
+        vatRate: vat_rate(all_data, price)
+      }]
     }
+
     processed_data
+  end
+
+  def vat_rate(all_data, price)
+    index = all_data.index(price)
+    case index
+    when 8
+      0
+    when 6
+      10
+    when 5
+      20
+    end
   end
 end
