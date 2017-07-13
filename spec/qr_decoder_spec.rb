0eb56bfbcb1b42754d60cb24e92741bd99dd393a
Pilar Huidobro <huidobropilar@gmail.com>
2017-07-13 11:05:51 +0200

    Correct and expand testing of QR code parsing

--- spec/qr_decoder_spec.rb
+++ spec/qr_decoder_spec.rb
@@ -2,34 +2,110 @@
 require_relative '../lib/qr_decoder'
 
 describe QRDecoder do
-  it 'detects that an image contains a QR code' do
-    image = Magick::Image.read('./spec/support/qr-test.pdf') { self.density = 600 }.first
+  describe '#qr_code?' do
+    it 'detects that an image contains a QR code' do
+      image = Magick::Image.read(
+        './spec/support/qr-test.pdf'
+      ) { self.density = 600 }.first
 
-    decoder = QRDecoder.new(image)
-    expect(decoder.qr_code?).to be true
+      decoder = QRDecoder.new(image)
+      expect(decoder.qr_code?).to be true
+    end
+
+    it 'detects that an image contains no QR code' do
+      image = Magick::Image.read(
+        './spec/support/no-qr-test.pdf'
+      ) { self.density = 600 }.first
+
+      decoder = QRDecoder.new(image)
+      expect(decoder.qr_code?).to be false
+    end
+
+    it 'does not manipulate the original image' do
+      image = Magick::Image.read(
+        './spec/support/qr-test.pdf'
+      ) { self.density = 600 }.first
+      original_image = image.dup
+
+      decoder = QRDecoder.new(image)
+      decoder.qr_code?
+
+      expect(image).to eq original_image
+    end
+
+    it 'does not process a non QR code symbol' do
+      image = Magick::Image.read(
+        './spec/support/barcode-test.pdf'
+      ) { self.density = 600 }.first
+
+      decoder = QRDecoder.new(image)
+      expect(decoder.qr_code?).to be false
+    end
   end
 
-  it 'detects that an image contains no QR code' do
-    image = Magick::Image.read('./spec/support/no-qr-test.pdf') { self.density = 600 }.first
+  describe '#decode_qr_code' do
+    it 'extracts the desired data from a QR code' do
+      image = Magick::Image.read(
+        './spec/support/qr-test.pdf'
+      ) { self.density = 600 }.first
 
-    decoder = QRDecoder.new(image)
-    expect(decoder.qr_code?).to be false
+      decoded_qr_code = QRDecoder.new(image).decode_qr_code
+
+      expect(decoded_qr_code).to(include :due_date) && :amounts
+    end
+
+    it 'extracts the date from a QR code' do
+      # From jEXku8e2rzutbmxeJ.pdf
+      image = Magick::Image.read(
+        './spec/support/qr-test.pdf'
+      ) { self.density = 600 }.first
+
+      decoded_qr_code = QRDecoder.new(image).decode_qr_code
+
+      expect(decoded_qr_code[:due_date]).to eq '2017-06-08T08:16:15'
+    end
+
+    it 'extracts the total and vat rate from a QR code' do
+      image = Magick::Image.read(
+        './spec/support/qr-test.pdf'
+      ) { self.density = 600 }.first
+
+      decoded_qr_code = QRDecoder.new(image).decode_qr_code
+
+      expect(decoded_qr_code[:amounts][:total]).to eq '6,90'
+      expect(decoded_qr_code[:amounts][:vat_rate]).to eq 0
+    end
   end
 
-  it 'does not manipulate the original image' do
-    image = Magick::Image.read('./spec/support/qr-test.pdf') { self.density = 600 }.first
-    original_image = image.dup
+  describe '#decode_qr_code' do
+    it 'extracts a 0% vat rate price from QR code data' do
+      image = Magick::Image.read(
+        './spec/support/qr-test.pdf'
+      ) { self.density = 600 }.first
 
-    decoder = QRDecoder.new(image)
-    decoder.qr_code?
+      decoded_qr_code = QRDecoder.new(image).decode_qr_code
+      expect(decoded_qr_code[:amounts][:total]).to eq '6,90'
+      expect(decoded_qr_code[:amounts][:vat_rate]).to eq 0
+    end
 
-    expect(image).to eq original_image
-  end
+    it 'extracts a 10% vat rate price from QR code data' do
+      image = Magick::Image.read(
+        './spec/support/10-percent-vat-qr-code.png'
+      ) { self.density = 600 }.first
 
-  it 'does not process a non QR code symbol' do
-    image = Magick::Image.read('./spec/support/barcode-test.pdf') { self.density = 600 }.first
+      decoded_qr_code = QRDecoder.new(image).decode_qr_code
+      expect(decoded_qr_code[:amounts][:total]).to eq '5,90'
+      expect(decoded_qr_code[:amounts][:vat_rate]).to eq 10
+    end
 
-    decoder = QRDecoder.new(image)
-    expect(decoder.qr_code?).to be false
+    it 'extracts a 20% vat rate price from QR code data' do
+      image = Magick::Image.read(
+        './spec/support/20-percent-vat-qr-code.pdf'
+      ) { self.density = 600 }.first
+
+      decoded_qr_code = QRDecoder.new(image).decode_qr_code
+      expect(decoded_qr_code[:amounts][:total]).to eq '60,72'
+      expect(decoded_qr_code[:amounts][:vat_rate]).to eq 20
+    end
   end
 end
