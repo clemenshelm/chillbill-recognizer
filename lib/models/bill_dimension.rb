ce786db7ca79849b423603993ba648fd075e6948
Pilar Huidobro <huidobropilar@gmail.com>
2017-07-19 11:11:24 +0200

    Introduce method for setting text box values

--- lib/models/bill_dimension.rb
+++ lib/models/bill_dimension.rb
@@ -8,7 +8,9 @@ class BillDimension < Sequel::Model
       BillDimension.create(name: 'bill_height', dimension: height)
     end
 
-    %w(bill_width bill_height).each do |dimension_name|
+    %w(
+      bill_width bill_height top_boundary bottom_boundary left_boundary right_boundary
+    ).each do |dimension_name|
       define_method(dimension_name) do
         BillDimension.find(name: dimension_name).dimension
       end
@@ -16,6 +18,15 @@ class BillDimension < Sequel::Model
 
     def bill_ratio
       bill_width / bill_height
+    end
+
+    def define_text_box(
+      top_boundary:, bottom_boundary:, left_boundary:, right_boundary:
+    )
+      BillDimension.create(name: 'top_boundary', dimension: top_boundary)
+      BillDimension.create(name: 'bottom_boundary', dimension: bottom_boundary)
+      BillDimension.create(name: 'left_boundary', dimension: left_boundary)
+      BillDimension.create(name: 'right_boundary', dimension: right_boundary)
     end
   end
 end
