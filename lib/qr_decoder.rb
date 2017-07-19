eaafd327562951e31876e788a87f355dbbb57efd
Pilar Huidobro <huidobropilar@gmail.com>
2017-07-19 19:35:01 +0200

    Rewrite qr code detection to be persistant

--- lib/qr_decoder.rb
+++ lib/qr_decoder.rb
@@ -16,28 +16,26 @@ class QRDecoder
   end
 
   def qr_code?
-    tmp_image = @image.dup
-    image_blob = tmp_image.to_blob do
-      self.depth = 8
-      self.format = 'PGM'
-    end
-    tmp_image.destroy!
-
-    @qr_codes = ZBar::Image.from_pgm(image_blob).process(symbology: :qrcode)
-    !@qr_codes.empty?
+    !qr_codes.empty?
   end
 
   def decode_qr_code
     return unless qr_code?
-    all_data = @qr_codes.last.instance_variable_get(:@data).split('_')
-    prices = all_data.find_all { |price| /\d,\d{2}/ =~ price }
+    all_data = qr_codes.last.data.split('_')
+    prices = all_data.grep(/\d,\d{2}/)
     prices_and_vats = VAT_RATES.zip(prices).to_h
     compacted_prices_and_vats = prices_and_vats.select do |_vat, price|
       /^(?!.*0,00).*$*\d,\d{2}/ =~ price
     end
-    processed_data = {
-      invoiceDate: DateTime.strptime(all_data[4], '%Y-%m-%d'),
-      dueDate: DateTime.strptime(all_data[4], '%Y-%m-%d'),
+
+    # Processed QR code data:
+    {
+      invoiceDate: DateTime.strptime(
+        all_data[4], '%Y-%m-%d'
+      ).strftime('%Y-%m-%d'),
+      dueDate: DateTime.strptime(
+        all_data[4], '%Y-%m-%d'
+      ).strftime('%Y-%m-%d'),
       amounts: [{
         total: (BigDecimal.new(
           compacted_prices_and_vats.values.first.sub!(',', '.')
@@ -46,6 +44,19 @@ class QRDecoder
         vatRate: compacted_prices_and_vats.keys.first
       }]
     }
-    processed_data
+  end
+
+  private
+
+  def qr_codes
+    return @qr_codes if @qr_codes
+    tmp_image = @image.dup
+    image_blob = tmp_image.to_blob do
+      self.depth = 8
+      self.format = 'PGM'
+    end
+    tmp_image.destroy!
+
+    @qr_codes = ZBar::Image.from_pgm(image_blob).process(symbology: :qrcode)
   end
 end
