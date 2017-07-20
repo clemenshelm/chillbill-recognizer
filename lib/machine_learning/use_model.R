4eff578a076bb6a6cc668cadd15b7cd5a2d48ac2
Thomas1e <thomas.le@gmx.at>
2017-07-20 14:37:31 +0200

    prepared usemodel for bill type classification

--- lib/machine_learning/use_model.R
+++ lib/machine_learning/use_model.R
@@ -1,33 +1,39 @@
 library(e1071, quietly = TRUE)
 library(jsonlite, quietly = TRUE)
-source('machine_learning_lib.R') # loads function "generate_tuples(price_list)"
+source("machine_learning_lib.R") # loads function "generate_tuples(price_list)"
 
 # load the bill (needs argument via command line like 296mm2mqrPTJMsB4J.csv)
-args = commandArgs(trailingOnly=TRUE)
-price_list = read.csv(args, header=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
+price_list <- read.csv(args, header = TRUE)
 
 # gnerate tuples and add attributes
-data_for_classification = generate_tuples(price_list)
+data_for_bill_type <- generate_bill_type_att(price_list)
+data_for_classification <- generate_tuples(price_list)
 
 
+# determine typ of bill
+bill_typ_classifier <- readRDS("modelfile_bill_typ_classifyer.rds")
+predicted_type <- predict(bill_typ_lassifier,
+                          data_for_bill_type,
+                          type = "C-classification")
 
-
-
-# load model
-M <- readRDS('modelfile.rds')
-
+# load correct model
+classification_model <- switch(predicted_type,
+                               "a4_letter" = readRDS("modelfile_a4_letter.rds"),
+                               "email" = readRDS("modelfile_email.rds"),
+                               "sales_check" = readRDS("modelfile_sales_check.rds"))
 
 # predict 
-p <- predict(M, data_for_classification , type = "C-classification")
+p <- predict(classification_model, data_for_classification, type = "C-classification")
 
 # Output
-tmp = data_for_classification[p == 1,]
-output = data.frame("price_id" = tmp$total_id, 
-                    "vat_id" = tmp$vat_id, 
-                    "total_price" = tmp$total_price, 
+tmp <- data_for_classification[p == 1, ]
+output <- data.frame("price_id" = tmp$total_id,
+                    "vat_id" = tmp$vat_id,
+                    "total_price" = tmp$total_price,
                     "vat_price" = tmp$vat_price,
                     "vat_rate" = tmp$vat_price / tmp$total_price * 100)
 
 
-toJSON(output, pretty=TRUE) # nice  formatted output
+toJSON(output, pretty = TRUE) # nice  formatted output
 #toJSON(output) # in one line
