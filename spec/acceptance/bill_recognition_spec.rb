95a083960ddb540c0c90ab6d2464dcec1e81a971
Pilar Huidobro <huidobropilar@gmail.com>
2017-08-16 13:15:36 +0200

    Revert changes

--- spec/acceptance/bill_recognition_spec.rb
+++ spec/acceptance/bill_recognition_spec.rb
@@ -2,12 +2,12 @@
 require_relative '../spec_cache_retriever'
 require_relative '../../lib/bill_recognizer'
 describe 'Recognizing bills correctly' do
-  it 'recognizes the bill BYnCDzw7nNMFergRW' do
-    retriever = SpecCacheRetriever.new(file_basename: 'WmcA2uThGP5QaaciP.pdf')
+  it 'recognizes the bill BYnCDzw7nNMFergRW', :focus do
+    retriever = SpecCacheRetriever.new(file_basename: 'BYnCDzw7nNMFergRW.pdf')
     recognizer = BillRecognizer.new(retriever: retriever)
 
     bill_attributes = recognizer.recognize
-    binding.pry
+
     expect(bill_attributes[:amounts]).to eq [{ total: 29_674, vatRate: 20 }]
     expect(bill_attributes[:currencyCode]).to eq 'EUR'
     expect(bill_attributes[:invoiceDate]).to eq '2016-03-16'
